066f28df01102808b91f3737c42d13e3b5827352
Carlos Fernandes <carlosamfernandes@gmail.com>
2020-04-06 17:20:07 -0300

    Criação e controle do banco de dados da aplicação

---
+++ db.js
@@ -0,0 +1,56 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./ws.db');
+
+db.serialize(function() {
+
+    //criar a tabela
+    db.run(`
+        CREATE TABLE IF NOT EXISTS ideas(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            title TEXT, 
+            category TEXT, 
+            description TEXT, 
+            link TEXT
+        ); 
+    `);
+
+    //inserir dados na tabela
+    /* const query = `
+    INSERT INTO ideas(
+        image,
+        title, 
+        category, 
+        description, 
+        link
+    ) VALUES(?,?,?,?,?);
+    `
+    //exemplo de inserção de dados
+    const values = [
+        "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+        "Cursos de Programação",
+        "Estudo",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quia illo corrupti veniam odit cumque nesciunt praesentium blanditiis similique distinctio. Delectus maiores nihil obcaecati provident praesentium sit, voluptate illum asperiores.",
+        "https://rocketseat.com.br"
+    ]
+
+    db.run(query, values, function(err) {
+        if (err) return console.log(err);
+        console.log(this);
+    }) */  
+
+    //deletar dados da tabela
+    /* db.run(`DELETE FROM ideas WHERE id=?`, [1], function(err) {
+        if(err) return console.log(err);
+        console.log("DELETADO", this);
+    }) */
+
+    //consultar dados na tabela
+    /* db.all(`SELECT * FROM ideas`, function(err, rows) {
+        if(err) return console.log(err);
+        console.log(rows);
+    }) */
+
+})
+
+module.exports = db;
